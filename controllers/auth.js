2b715acaa9efca53fbcfb88b745697b122f7dad2
Cihan <ozcancihan714@gmail.com>
2023-07-14 15:46:27 +0300

    login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const CustomError = require("../helpers/error/CustomError");
 const errorWrapper = require("express-async-handler");
 const User = require("../models/User");
+const { isMatchPassword } = require("../helpers/auth/authHelpers");
 
 const register = errorWrapper(async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,8 +13,26 @@ const register = errorWrapper(async (req, res, next) => {
   });
 
   return res.status(201).json({
-    user,
+    user: { ...user._doc, password: undefined },
   });
 });
 
-module.exports = { register };
+const login = errorWrapper(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username }).select("+password");
+
+  if (!user)
+    return next(
+      new CustomError("There is no user with this email address.", 404)
+    );
+
+  if (!isMatchPassword(password, user.password))
+    return next(new CustomError("Wrong password!", 400));
+
+  return res.status(200).json({
+    user: { ...user._doc, password: undefined },
+  });
+});
+
+module.exports = { register, login };
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 require("dotenv").config();
 const connectDB = require("./helpers/db/connectDB");
 const routers = require("./routers");
+const errorHandler = require("./middlewares/error/errorHandler");
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,6 +11,7 @@ connectDB();
 
 app.use(express.json());
 app.use("/api", routers);
+app.use(errorHandler);
 
 app.listen(
   PORT,
